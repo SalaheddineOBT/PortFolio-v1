cbd0b267ec449d8b43362fbf52bae44694cd7733
SalaheddineOBT <91952603+SalaheddineOBT@users.noreply.github.com>
2022-04-27 14:01:36 +0000

    save changes

---
+++ js/JavaScript.js
@@ -0,0 +1,27 @@
+﻿$(document).ready(function () {
+
+    //scroll top ----------------------------------------------
+
+    function ScrollToping() {
+        const scrollTop = document.getElementById("toping");
+        if (this.scrollY >= 160)
+            scrollTop.classList.add('toping2');
+        else
+            scrollTop.classList.remove('toping2');
+    }
+    window.addEventListener('scroll', ScrollToping);
+   
+});
+
+//Vanilla Code ---------------------------------------------
+
+VanillaTilt.init(document.querySelectorAll('.titl'), {
+    max: 10
+});
+
+//scroll smooth --------------------------------------------
+
+var scroll = new SmoothScroll('a[href*="#"]', {
+	speed: 1000,
+	speedAsDuration: true
+});
